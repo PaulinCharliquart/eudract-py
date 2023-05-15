0659c92c3fd7c6c1bd95902eb5ccfc96b1471286
PaulinCharliquart <64043642+PaulinCharliquart@users.noreply.github.com>
2023-05-15 21:15:35 +0000

    test: use Pathlib

--- tests/test_main.py
+++ tests/test_main.py
@@ -2,57 +2,41 @@ from eudract import Eudract
 import tempfile
 import sqlite3
 import json
+from pathlib import Path
 
 
 def test_search():
     eu = Eudract()
     x = eu.search("EFC14280", "summary", False)
-    x[0] = x[0].replace("\r", "")
-    x[0] = x[0].replace("\n", "")
-    with open("tests/data/one_study_summary.txt") as f:
-        y = f.read()
-    assert x[0] == y
+    x[0] = x[0].replace("\r", "").replace("\n", "")
+    assert x[0] == Path("tests/data/one_study_summary.txt").read_text()
     x = eu.search("EFC14280", "summary", True)
-    with open("tests/data/one_study_summary.json") as f:
-        y = f.read()
-    assert json.dumps(x[0]) == y
+    assert json.dumps(x[0]) == Path("tests/data/one_study_summary.json").read_text()
     assert eu.search("gutihkkùml") == []
     x = eu.search("covid", size=10, to_dict=True)
     assert len(x) == 10
     assert len(set([i["EudraCT Number"] for i in x])) == 10
     x = eu.search("EFC14280", "full", to_dict=True)
-    with open("tests/data/full.json") as f:
-        y = f.read()
-    assert json.dumps(x) == y
+    assert json.dumps(x) == Path("tests/data/full.json").read_text()
 
 
 def test_info():
     eu = Eudract()
     assert eu.info("jffs") == ""
     x = eu.info("2015-001314-10", "summary", False)
-    x = x.replace("\r", "")
-    x = x.replace("\n", "")
-    with open("tests/data/one_study_summary.txt") as f:
-        y = f.read()
-    assert x == y
+    x = x.replace("\r", "").replace("\n", "")
+    assert x == Path("tests/data/one_study_summary.txt").read_text()
     x = eu.info("2015-001314-10", "summary", True)
-    with open("tests/data/one_study_summary.json") as f:
-        y = f.read()
-    assert json.dumps(x) == y
+    assert json.dumps(x) == Path("tests/data/one_study_summary.json").read_text()
 
 
 def test_info_full():
     eu = Eudract()
     x = eu.info("2015-001314-10", "full", to_dict=False)
-    x = x.replace("\r", "")
-    x = x.replace("\n", "")
-    with open("tests/data/one_study_full.txt") as f:
-        y = f.read()
-    assert x == y
+    x = x.replace("\r", "").replace("\n", "")
+    assert x == Path("tests/data/one_study_full.txt").read_text()
     x = eu.info("2015-001314-10", "full", to_dict=True)
-    with open("tests/data/one_study_full.json") as f:
-        y = f.read()
-    assert json.dumps(x) == y
+    assert json.dumps(x) == Path("tests/data/one_study_full.json").read_text()
 
 
 def test_search_cache():
