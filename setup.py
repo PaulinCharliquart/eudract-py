20a10911e32ff70e0084725ec4be4772c582af89
Paulin Charliquart <paulincharliquart@gmail.com>
2023-07-14 22:56:15 +0200

    fix ci

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
     },
     install_requires=["requests", "bs4"],
     extras_require={
-        "dev": ["black", "pytest", "flake8", "furo", "sphinx", "myst-parser"],
+        "dev": ["black", "pytest", "flake8", "furo", "sphinx", "myst-parser", "sphinx_copybutton"],
     },
     include_package_data=True,
     zip_safe=False,
