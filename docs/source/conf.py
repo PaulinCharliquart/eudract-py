0e2b3b490fb1ff63a150fc5cfe0d842fa851f4ea
PaulinCharliquart <64043642+PaulinCharliquart@users.noreply.github.com>
2023-07-14 23:11:35 +0200

    Test doc (#4)
    * feat: add workflow to publish doc

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -35,6 +35,7 @@ extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.napoleon",
     "myst_parser",
+    "sphinx_copybutton",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
